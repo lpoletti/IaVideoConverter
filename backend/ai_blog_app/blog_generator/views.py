aaa611d48a6b29e4c76cdb70b0dd766dbc78b7d7
lpoletti <luanpoletti@gmail.com>
2024-09-28 17:20:21 -0300

    signup flow created

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 # Create your views here.
 # @login_required(login_url="login")
 def index(request):
@@ -10,4 +12,22 @@ def user_login(request):
     return render(request,'login.html')
 
 def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email= request.POST['email']
+        password = request.POST['senha']
+        repeatPass = request.POST['repeatSenha']
+        if password == repeatPass:
+            try:
+                user = User.objects.create_user(username,email,password)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except:
+                error_message = 'Erro ao criar o usuário'
+                return render(request,'signup.html', {error_messsage:error_message})
+
+        else:
+            error_message = "Senhas não são iguais"
+            return render(request,'signup.html', {error_messsage:error_message})
     return render(request,'signup.html')
